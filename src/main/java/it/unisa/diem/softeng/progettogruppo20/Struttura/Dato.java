9c40a1bbac860bfefd0eb74bc4697750dc88b4c0
acest <m.donofrio33@studenti.unisa.it>
2024-12-06 16:06:27 +0100

    MATTEO D'ONOFRIO Commenti doxygen aggiornati per le classi Dato, Email e Telefono.

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/Struttura/Dato.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/Struttura/Dato.java
@@ -5,14 +5,11 @@ import java.util.List;
 
 /**
  * @file Dato.java
- * @brief Classe astratta che contiene i metodi per modificare un oggetto di
+ * @brief Classe astratta che contiene i metodi per modificare gli attributi di un oggetto di
  * tipo Dato.
  *
  * Questa classe ha come attributo una Lista e contitene i metodi astratti che
- * saranno riscritti dalle classi Email e Telefono, ed essendo una classe
- * astratta si potrà aggiungere alla classe Contatto qualsiasi attributo
- * creandone una relativa classe che estende questa cclasse e ne riscrive i
- * metodi astratti.
+ * saranno riscritti dalle classi Email e Telefono.
  *
  * @author Matteo D'Onofrio
  * @date December 06, 2024
@@ -23,18 +20,17 @@ public abstract class Dato {
     private List<String> dati;
 
     /**
-     * @brief Costruttore. Crea una LinkedList di stringhe chiamata dati.
+     * @brief Costruttore. 
+     * 
+     * Crea una LinkedList di stringhe chiamata dati.
      */
     public Dato() {
         dati = new LinkedList<String>();
     }
 
     /**
-     * @brief metodo che controlla che il dato inserito in input sia contenuto
-     * nella lista.
-     *
-     * Questo metodo scorre la LinkedList e restituisce true se la stringa
-     * inserita in input è contenuta in essa altrimeti false.
+     * @brief metodo che controlla che la Stringa inserita in input sia
+     * contenuta nella lista.
      *
      * @pre La lista non è vuota.
      * @post Viene restituito un booleano.
@@ -42,14 +38,16 @@ public abstract class Dato {
      * @param[in] dato La stringa della quale verificare l'esistenza all'interno
      * della LinkedList.
      *
-     * @return true o false.
+     * @return true se dato è presente altrimenti false.
      */
-    private boolean isContenuto(String dato) {
+    private boolean contiene(String dato) {
         return true;
     }
 
     /**
      * @brief metodo che aggiunge un dato all'interno della lista.
+     *
+     * See also: limiteRaggiunto()
      *
      * Questo metodo controlla se non è stato raggiunto il limite di 3 elementi
      * grazie al metodo limiteRaggiunto e in tal caso esegue una add del dato
@@ -58,7 +56,7 @@ public abstract class Dato {
      * @post Verrà modificata la LinkedList.
      * @param[in] dato La stringa da aggiungere alla LinkedList.
      * @see limiteRaggiunto()
-     * @return true o false.
+     * @return true se l'aggiunta è andata a buon fine altrimenti false.
      */
     protected boolean aggiungiDato(String dato) {
         return true;
@@ -70,7 +68,7 @@ public abstract class Dato {
      * Questo metodo controlla che il dato sia contenuto nella lista e in tal
      * caso lo rimuove con una remove.
      *
-     * @post Viene eliminato il dato se presente.
+     * @post Viene eliminato il dato.
      * @param[in] dato La stringa da eliminare dalla LinkedList.
      */
     protected void rimuoviDato(String dato) {
@@ -78,7 +76,9 @@ public abstract class Dato {
     }
 
     /**
-     * @brief metodo che modifica un dato all'interno della lista.
+     * @brief metodo astratto che modifica un dato all'interno della lista.
+     *
+     * See also: aggiungiDato(), rimuoviDato()
      *
      * Questo metodo prende in input il dato vecchio, lo elimina grazie al
      * metodo rimuoviDato() e al suo posto aggiunge il dato nuovo preso in input
@@ -88,39 +88,32 @@ public abstract class Dato {
      * @post La lista avrà uno dei suoi parametri modificato.
      * @invariant La dimensione della lista deve rimanere invariata.
      * @param[in] datoVecchio Il dato da eliminare dalla lista.
-     * @param[in] datoVecchio Il dato da aggiungere alla lista.
+     * @param[in] datoNuovo Il dato da aggiungere alla lista.
      * @see aggiungiDato()
      * @see rimuoviDato()
-     * @return true o false.
+     * @return true se la modifica è andata a buon fine altrimenti false.
      */
     protected abstract boolean modificaDato(String datoNuovo, String datoVecchio);
 
     /**
-     * @brief metodo che controlla se è stato superato il massimo di dati.
-     *
-     * Questo metodo controlla la dimensione della lista se è uguale a 3
-     * restistuisce true altrimenti false.
+     * @brief metodo che controlla se è stato raggiunto il massimo di 3 stringhe
+     * all'interno della lista.
      *
      * @post Viene restituito un booleano.
      * @invariant La lista non deve essere modificata.
-     * @return true o false.
+     * @return true se il limite è stato raggiunto altrimenti false.
      */
     private boolean limiteRaggiunto() {
         return true;
     }
 
     /**
-     * @brief metodo che restituisce il tipo del dato.
-     *
-     * Questo metodo se è fa rifereimento ad un oggetto della classe Email
-     * restitusice "Email", altrimenti se fa riferimento ad un oggetto della
-     * classe Telefono restuisice "Telefono".
+     * @brief metodo astratto che restituisce il tipo del dato.
      *
      * @pre La lista non è vuota.
-     * @post Il metodo chiamante saprà di che tipo è il dato presente
-     * nell'oggetto di tipo Contatto.
-     * @invariant La lista non è vuota.
-     * @return una stringa contenente "Email" o "Telefono".
+     * @post Il metodo chiamante ottiene il tipo del dato
+     * @return una stringa contenente "Email" se si tratta di un oggetto della
+     * classe Email altrimenti "Telefono".
      */
     protected abstract String getTipo();
 
