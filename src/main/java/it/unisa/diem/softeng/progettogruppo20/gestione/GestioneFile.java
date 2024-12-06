774ab9c003864a00504466fda7b98684c9636190
LuigiContaldo <l.contaldo1@studenti.unisa.it>
2024-12-06 11:14:02 +0100

    LUIGI CONTALDO Spostamento gestione file nel package gestione

---
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/GestioneFile.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.softeng.progettogruppo20.gestione;
+
+import gestione.ListaContatti;
+
+/**
+ *
+ * @author luigicocco
+ */
+public class GestioneFile {
+    
+    private String fileName;
+    
+    public GestioneFile(String fileName){
+        this.fileName = fileName;
+        
+    }
+    
+    public void esporta(){
+        
+    }
+    
+    public ListaContatti importa(){
+        
+        return null;
+    }
+    
+    
+    
+}
