19a2136e8dedf237cd4937a735047f1cca6b1105
LuigiCocco <l.cocco1@studenti.unisa.it>
2024-12-06 13:11:42 +0100

    Luigi Cocco commenti doxygen per GestioneFile

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/GestioneFile.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/GestioneFile.java
@@ -1,16 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file GestioneFile.java
+ * @brief si occupa di salvare ed importare la lista dei contatti
+ * 
+ * @author Luigi Cocco
+ * @date December 6, 2024
+ * @version 1.0
  */
+
 package it.unisa.diem.softeng.progettogruppo20.gestione;
 
 import it.unisa.diem.softeng.progettogruppo20.gestione.ListaContatti;
 
-/**
- *
- * @author luigicocco
- */
+
 public class GestioneFile {
     
     private String fileName;
@@ -20,10 +21,24 @@ public class GestioneFile {
         
     }
     
-    public void esporta(){
+    /**
+     * @brief salva la lista dei contatti in un file
+     * @pre listaContatti non dev'essere vuota
+     * @post viene creato il file(con nome filename) contente la listaContatti
+     * @invariant la lista passata non è modificata
+     * 
+     * @param[in] listaContatti è la lista che dev'essere salvata nel file
+     */
+    public void esporta(ListaContatti listaContatti){
         
     }
     
+    /**
+     * @brief importa dal file denominato filename la listaContatti
+     * @pre il file denominato filename non dev'essere vuoto
+     * @post deve aver importato la listaContatti dal file
+     * @return ritorna la listaContatti di ListaContatti
+     */
     public ListaContatti importa(){
         
         return null;
