e989ba555fac8ac95c64d0f588b55268e98eef4d
acest <m.donofrio33@studenti.unisa.it>
2024-12-06 16:10:21 +0100

    MATTEO D'ONOFRIO Commit risoluzione conflitti

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/Struttura/Contatto.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/Struttura/Contatto.java
@@ -1,20 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file Contatto.java
+ * @brief si occupa della creazione e gestione di un oggetto di Contatto
+ * 
+ * per l'implementazione dell'ordinamento è stato utilizzato un compareTo(..) implementando l'interfaccia Comparable
+ * 
+ * @author Luigi Cocco
+ * @date December 6, 2024
+ * @version 1.0
  */
 package it.unisa.diem.softeng.progettogruppo20.Struttura;
 
-/**
- *
- * @author luigicocco
- */
+
 public class Contatto implements Comparable{
     private String nome;
     private String cognome;
     private Dato tel;
     private Dato email;
     
+    /**
+     * @brief Crea un oggetto di tipo Contatto
+     * 
+     * See also: Telefono(), Email(), aggiungiDato()
+     * 
+     * Per email e telefono chiama aggiungiDato(..) 
+     * 
+     * @pre 'nome!=null || cognome!=null' non possono essere entrambi null
+     * @post L'oggetto contatto è stato creato
+     * 
+     * 
+     * @param[in] nome è il nome del Contatto
+     * @param[in] cognome è il cognome del Contatto
+     * @param[in] telefono è il numero del Contatto
+     * @param[in] email è l'email del Contatto
+     * @see Telefono()
+     * @see Email()
+     * @see aggiungiDato()
+     */
     public Contatto(String nome, String cognome, String telefono, String email){
         this.nome = nome;
         this.cognome = cognome;
@@ -26,11 +47,35 @@ public class Contatto implements Comparable{
         
     }
     
+
+    
+    
     @Override
     public int compareTo(Object o){
         return 0;
     }
     
+    /**
+     * @brief modifica il contatto attuale con i parametri nuovi
+     * 
+     * See also: modificaDato()
+     * 
+     * Se il parametroNuovo è non nullo allora sovrascrive il parametroVecchio
+     * Se devono essere modificate l'emailVecchia oppure il telefonoVecchio allora viene invocato modificaDato(..)
+     * 
+     * @pre '   nomeNuovo!=null || cognomeNuovo!=null || emailNuova!=null || telefonoNuovo!=null   '
+     * @post il contatto è stato modificato
+     * @invariant i dati del contatto ai quali sono associati (parametroNuovo == null) non devono essere modificati 
+     * 
+     * @param[in] nomeNuovo sovrascrive il nome dell'istanza corrente
+     * @param[in] cognomeNuovo sovrascrive il cognome dell'istanza corrente
+     * @param[in] emailVecchia è l'email che si vuole sovrascrivere
+     * @param[in] emailNuova è l'email che sovrascrive emailVecchia
+     * @param[in] telefonoVecchio è il numero di telefono che si vuole sovrascrivere
+     * @param[in] telefonoNuovo è il numero di telefono che sovrascrive telefonoVecchio
+     * @see modificaDato()
+     * @return se la modifica è avvenuta con successo ritorna true, altrimenti false
+     */
     protected boolean modifica(String nomeNuovo, String cognomeNuovo, String emailVecchia, String emailNuova, String telefonoVecchio, String telefonoNuovo){
         
         
--- src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/GestioneFile.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/GestioneFile.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file GestioneFile.java
+ * @brief si occupa di salvare ed importare la lista dei contatti
+ * 
+ * @author Luigi Cocco
+ * @date December 6, 2024
+ * @version 1.0
  */
+
 package it.unisa.diem.softeng.progettogruppo20.gestione;
 
 /**
  *
  * @author luigicocco
  */
+import it.unisa.diem.softeng.progettogruppo20.gestione.ListaContatti;
+
+
 public class GestioneFile {
     
     private String fileName;
@@ -18,10 +25,24 @@ public class GestioneFile {
         
     }
     
-    public void esporta(){
+    /**
+     * @brief salva la lista dei contatti in un file
+     * @pre listaContatti non dev'essere vuota
+     * @post viene creato il file(con nome filename) contente la listaContatti
+     * @invariant la lista non viene modificata
+     * 
+     * @param[in] listaContatti è la lista che dev'essere salvata nel file
+     */
+    public void esporta(ListaContatti listaContatti){
         
     }
     
+    /**
+     * @brief importa dal file denominato filename la listaContatti
+     * @pre il file denominato filename non dev'essere vuoto
+     * @post si otterrà un oggetto di ListaContatti
+     * @return ritorna un oggetto di ListaContatti
+     */
     public ListaContatti importa(){
         
         return null;
