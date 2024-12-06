f9300d1cea0413d6ff0416e947abc8c6172dc0ca
LuigiCocco <l.cocco1@studenti.unisa.it>
2024-12-06 11:03:39 +0100

    Luigi Cocco creazione della classe GestioneFile

---
+++ src/main/java/gestione/GestioneFile.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestione;
+
+/**
+ *
+ * @author luigicocco
+ */
+public class GestioneFile {
+    
+    private String fileName;
+    
+    public GestioneFile(String fileName){
+        this.fileName = fileName;
+        
+    }
+    
+    public void esporta(){
+        
+    }
+    
+    public ListaContatti importa(){
+        
+        return null;
+    }
+    
+    
+    
+}
