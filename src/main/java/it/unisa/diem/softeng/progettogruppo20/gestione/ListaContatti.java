b5193f05609f913663b9f9b1368812a6de4ea1bf
fabrizioderr <f.derrico2@studenti.unisa.it>
2024-12-06 11:23:20 +0100

    FABRIZIO D'ERRICO creazione classe ListaContatti e relativi metodi

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/ListaContatti.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/ListaContatti.java
@@ -5,10 +5,41 @@
  */
 package it.unisa.diem.softeng.progettogruppo20.gestione;
 
+import it.unisa.diem.softeng.progettogruppo20.Struttura.Contatto;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
- * @author acest
+ * @author fabrizio d'errico
  */
 public class ListaContatti {
-    
+
+    private Set<Contatto> elenco;
+
+    public ListaContatti() {
+        this.elenco = new TreeSet<Contatto>();
+    }
+
+    /*
+        public Set<Contatto> getElenco(){
+        return elenco;
+        }
+     */
+    public boolean aggiungiContatto(String nome, String cognome, String telefono, String email) {
+
+        return true;
+    }
+
+    public ListaContatti cercaContatto(String nome, String cognome) {
+        return null;
+    }
+
+    public boolean rimuoviContatto(Contatto c) {
+        return true;
+    }
+
+    public boolean modificaContatto(String nomeVecchio, String nomeNuovo, String cognomeVecchio, String cognomeNuovo, String emailVecchia, String emailNuova, String telefonoVecchio, String telefonoNuovo) {
+        return true;
+    }
 }
