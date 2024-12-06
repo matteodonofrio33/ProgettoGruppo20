adad703fdf87928d085bf3bf6325bce812762c27
LuigiContaldo <l.contaldo1@studenti.unisa.it>
2024-12-06 13:16:27 +0100

    LUIGI CONTALDO Aggiunta commenti Doxygen per classe Gui e Rubrica

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/Rubrica.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/gestione/Rubrica.java
@@ -8,23 +8,53 @@ package it.unisa.diem.softeng.progettogruppo20.gestione;
 import it.unisa.diem.softeng.progettogruppo20.interfaccia.Gui;
 
 /**
+ * @file Rubrica.java
+ * @brief Si occupa di salvare la lista dei contatti e richiamare il metodo
+ * iniziale per avviare il programma.
  *
- * @author LuigiContaldo
+ * Questa classe contiene un attributo di tipo ListaContatti per contenere la
+ * lista di tutti i contatti. Una lista di tipo ListaContatti che contiene solo
+ * la lista che ci restituisce il metodo cercaContatto.
+ *
+ * @author Luigi Contaldo
+ * @data December 06, 2024
+ * @version 1.0
  */
 public class Rubrica {
-    
+
     private ListaContatti lista;
     private ListaContatti trovati;
     private Gui gui;
-    
-    public Rubrica(ListaContatti lista, ListaContatti trovati, Gui gui){
-        
+
+    /**
+     * @brief Costruttore
+     *
+     * @param[in] lista Riferimento all'oggetto ListaContatti. Contiene tutti i
+     * contatti della rubrica.
+     *
+     * @param[in] trovati Riferimento all'oggetto ListaContatti. Contiene tutti
+     * i contatti restituiti dalla funzione cercaContatto().
+     * @param[in] gui Riferimento all'oggetto Gui.
+     *
+     */
+    public Rubrica(ListaContatti lista, ListaContatti trovati, Gui gui) {
+
+        this.lista = lista;
         this.trovati = trovati;
         this.gui = gui;
     }
-    
-    private void vista(){
-        
+
+    /**
+     * @brief Invoca tutti i metodi necessari per la visualizzazione della
+     * rubrica.
+     *
+     * @pre La scena principale deve esistere e deve essere vuota.
+     *
+     * @post Invoca i metodi necessari per visualizzare correttamente tutta la
+     * schermata della rubrica.
+     */
+    private void vista() {
+
     }
-    
+
 }
