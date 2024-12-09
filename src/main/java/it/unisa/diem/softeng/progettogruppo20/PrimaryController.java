a2febef9d03fbc436caedc950f51f40e547dedc7
LuigiCocco <l.cocco1@studenti.unisa.it>
2024-12-09 11:45:25 +0100

    Luigi Cocco PrimaryController dichiarazione di bottoni, campi di testo, lista osservabile ecc...

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
@@ -1,12 +1,78 @@
 package it.unisa.diem.softeng.progettogruppo20;
 
+import it.unisa.diem.softeng.progettogruppo20.Struttura.Contatto;
+import java.awt.Button;
+import java.awt.TextField;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
+    
+    
+    
+    //dichiarazione bottoni presenti nell'interfaccia:
+    @FXML
+    private TextField nomeTfd;
+    
+    @FXML
+    private TextField cognomeTfd;
+    
+    @FXML
+    private TextField telefonoTfd;
+    
+    @FXML
+    private Button aggiungiBtn;
+    
+    @FXML
+    private Button rimuoviBtn;
+    
+    
+    //dichiarazione tabella:
+    @FXML
+    private TableView<Contatto> tabellaContatti;
+    
+    //colonne per ogni campo del contatto:
+    
+    @FXML
+    private TableColumn<Contatto, String> nomeClm;
+    
+    @FXML
+    private TableColumn<Contatto, String> cognomeClm;
+    
+     @FXML
+    private TableColumn<Contatto, String> telefonoClm;
+    
+    //lista osservabile
+    
+    private ObservableList<Contatto> contatti;
+    
+    
+    
+    
+    
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        contatti = FXCollections.observableArrayList();
+       
+    }
+    
+    
+    
+    
+    
+    
 }
