c33f2029f20c1838a1c9c6fdd1b3822d02ae7ec8
LuigiCocco <l.cocco1@studenti.unisa.it>
2024-12-10 11:09:51 +0100

    Luigi Cocco  commit prima di fare merge con risoluzione javafx

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
@@ -122,12 +122,13 @@ public class PrimaryController implements Initializable {
     private void updateTableView(){
         contatti.setAll(listaContatti.getElenco());
     }
-    
+   
     @FXML
     private void addContact(ActionEvent event) {
-        
+       
         Contatto nuovoContatto=listaContatti.aggiungiContatto(nomeTfd.getText(), cognomeTfd.getText(), telTfd1.getText(), telTfd2.getText(), telTfd3.getText(), emailTfd1.getText(), emailTfd2.getText(), emailTfd3.getText());
         
     }
+
 
 }
