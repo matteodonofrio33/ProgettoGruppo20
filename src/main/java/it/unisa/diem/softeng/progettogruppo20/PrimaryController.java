8bcac0981ab87cf9445c8bf74ecc0c4f35a2a0bd
fabrizioderr <f.derrico2@studenti.unisa.it>
2024-12-11 17:26:32 +0100

    FABRIZIO D'ERRICO Merge con branch Sviluppo.

--- src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
+++ src/main/java/it/unisa/diem/softeng/progettogruppo20/PrimaryController.java
@@ -1,9 +1,11 @@
 package it.unisa.diem.softeng.progettogruppo20;
 
 import it.unisa.diem.softeng.progettogruppo20.Struttura.Contatto;
+import it.unisa.diem.softeng.progettogruppo20.gestione.GestioneFile;
 import it.unisa.diem.softeng.progettogruppo20.gestione.ListaContatti;
 import java.awt.Button;
 import java.awt.TextField;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
@@ -20,6 +22,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class PrimaryController implements Initializable {
 
@@ -92,8 +96,8 @@ public class PrimaryController implements Initializable {
     //lista osservabile
     private ListaContatti listaContatti;
     private ObservableList<Contatto> contatti;
-   // private ObservableList<Contatto> listaFiltrata;
-    
+    // private ObservableList<Contatto> listaFiltrata;
+
 
     /* private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
@@ -121,54 +125,53 @@ public class PrimaryController implements Initializable {
         contatti.setAll(listaContatti.getElenco());
     }
 
-    private void controllerBottoneAggiungi(){
+    private void controllerBottoneAggiungi() {
         BooleanBinding cond1 = Bindings.isEmpty(nomeTfd.textProperty());
         BooleanBinding cond2 = Bindings.isEmpty(cognomeTfd.textProperty());
         aggiungiBtn.disableProperty().bind(Bindings.and(cond1, cond2));
     }
-    
+
     @FXML
     private void modContatto(ActionEvent event) {
-        
+
         Contatto selezione = tabellaContatti.getSelectionModel().getSelectedItem();
-        
+
         nomeTfd.textProperty().set(selezione.getNome());
         cognomeTfd.textProperty().set(selezione.getCognome());
-        
+
         telTfd1.textProperty().set(selezione.getTel().getDati().get(0));
         telTfd2.textProperty().set(selezione.getTel().getDati().get(1));
         telTfd3.textProperty().set(selezione.getTel().getDati().get(2));
-        
+
         emailTfd1.textProperty().set(selezione.getEmail().getDati().get(0));
         emailTfd2.textProperty().set(selezione.getEmail().getDati().get(1));
         emailTfd3.textProperty().set(selezione.getEmail().getDati().get(2));
-         
+
     }
-    
-   @FXML
-    private void confermaModifica(){
-        
+
+    @FXML
+    private void confermaModifica() {
+
         Contatto selezione = tabellaContatti.getSelectionModel().getSelectedItem();
-        
+
         listaContatti.modificaContatto(selezione, nomeTfd.getText(), cognomeTfd.getText(), telTfd1.getText(), telTfd2.getText(), telTfd3.getText(), emailTfd1.getText(), emailTfd2.getText(), emailTfd3.getText());
         aggiornamentoTableView();
-        
+
         puliziaCampi();
     }
 
     @FXML
     private void cercaContatto(ActionEvent event) {
-        String nome=nomeTfd.getText();
-        String cognome=cognomeTfd.getText();
+        String nome = nomeTfd.getText();
+        String cognome = cognomeTfd.getText();
         ListaContatti listaCercata = new ListaContatti();
         listaCercata = listaContatti.cercaContatto(nome, cognome);
-        System.out.println("HO CERCATO: " +listaCercata);
-        
-        contatti.setAll(listaCercata.getElenco());
-       // contatti= FXCollections.observableArrayList();
-       // tabellaContatti.setItems(contatti);
-    }
+        System.out.println("HO CERCATO: " + listaCercata);
 
+        contatti.setAll(listaCercata.getElenco());
+        // contatti= FXCollections.observableArrayList();
+        // tabellaContatti.setItems(contatti);
+    }
 
     @FXML
     private void addContact() {
@@ -206,7 +209,28 @@ public class PrimaryController implements Initializable {
     private void chiusuraProgramma() {
         Platform.exit();
     }
-    
-    
+
+    @FXML
+    private void salvaFileCSV() throws IOException {
+        // Crea il file chooser per selezionare il percorso di salvataggio
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("File CSV", "*.csv"));
+        fileChooser.setTitle("Salva il file");
+
+        // Mostra la finestra di salvataggio e ottieni il file selezionato
+        File file = fileChooser.showSaveDialog(new Stage());
+
+        // Verifica che il file non sia null
+        if (file != null) {
+            // Crea l'oggetto GestioneFile passando il percorso del file
+            GestioneFile gf = new GestioneFile(file.getAbsolutePath());
+
+            // Esporta i dati della listaContatti nel file CSV
+            gf.esporta(listaContatti);
+            System.out.println("ESPORTAZIONE RIUSCITA");
+        } else {
+            System.out.println("File non selezionato");
+        }
+    }
 
 }
